428da5189a2aac36eed3dda9a22c2f9660d98e16
Ali sami hakemy <47300221+alihakemy@users.noreply.github.com>
2023-02-02 20:42:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const NodeMediaServer = require('node-media-server');
 
 const config = {
   rtmp: {
-    port: `0.0.0.0:$PORT`,
+    port:process.env.PORT,
     chunk_size: 60000,
     gop_cache: true,
     ping: 30,
     ping_timeout: 60
   },
   http: {
-    port:`0.0.0.0:$PORT`,
+    port:process.env.PORT,
     allow_origin: '*'
   }
 };
